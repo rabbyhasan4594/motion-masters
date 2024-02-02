0c61b2a0e03a2f81e4d82e4e4533e1d300855b19
rabbyhasan <rabbyhasan4594@gmail.com>
2024-02-02 23:41:17 +0600

    title change

--- src/component/SectionTitle/SectionTitle.jsx
+++ src/component/SectionTitle/SectionTitle.jsx
@@ -2,9 +2,9 @@
 
 const SectionTitle = ({heading}) => {
     return (
-        <div className="mx-auto text-center md:w-4/12 my-8">
+        <div className="mx-auto text-center md:w-4/12  my-8 ">
             
-            <h3 className="text-3xl uppercase border-y-4 border-sky-950 py-4">{heading}</h3>
+            <h3 className="lg:text-3xl text-2xl uppercase border-4 text-white bg-slate-500 border-slate-600 py-4">{heading}</h3>
         </div>
     );
 };
